90a5530f37d44775031c520d5305c468d3b81c1f
manan-bedi2908 <manan.19535@sscbs.du.ac.in>
2021-12-28 22:19:35 +0530

    add artifacts

--- src/training.py
+++ src/training.py
@@ -1,8 +1,9 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model, save_model
+from src.utils.model import create_model, save_model, save_plot
 import argparse
 import os
+import pandas as pd
 
 
 def training(config_path):
@@ -23,8 +24,11 @@ def training(config_path):
     model_dir_path = os.path.join(artifacts_dir, model_dir)
     os.makedirs(model_dir_path, exist_ok=True)
     save_model(model, model_name, model_dir_path)
-
-
+    plot_name = config["artifacts"]["plot_name"]
+    plots_dir = config["artifacts"]["plots_dir"]
+    plot_dir_path = os.path.join(artifacts_dir, plots_dir)
+    os.makedirs(plot_dir_path, exist_ok=True)
+    save_plot(pd.DataFrame(history.history), plot_name, plot_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import time
 import os
+import matplotlib.pyplot as plt
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
     LAYERS = [
@@ -22,3 +23,14 @@ def save_model(model, model_name, model_dir):
     unique_filename = get_unique_filename(model_name)
     path_to_model = os.path.join(model_dir, unique_filename)
     model.save(path_to_model)
+
+def get_unique_plotname(plot_name):
+    unique_plotname = time.strftime(f"%Y%m%d_%H%M%S_{plot_name}")
+    return unique_plotname
+
+def save_plot(df, plot_name, plot_dir):
+    unique_plotname = get_unique_plotname(plot_name)
+    df.plot(figsize=(10,7))
+    plt.grid(True)
+    path_to_plot = os.path.join(plot_dir, unique_plotname)
+    plt.savefig(path_to_plot)
